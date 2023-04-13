01d8e2376761db2c9aca9522ae19bed795709ed8
rocen <rocen9609@163.com>
2023-04-13 21:37:29 +0800

    chore: 删除package-lock.json

--- components/Markdown/index.jsx
+++ components/Markdown/index.jsx
@@ -5,38 +5,40 @@ import remarkGfm from "remark-gfm";
 import HeadingBlock from "../Heading/block";
 
 const Markdown = ({ content }) => {
-    const components = {
-      code({ node, inline, className, children, ...props }) {
-        const match = /language-(\w+)/.exec(className || "");
-  
-        return !inline && match ? (
-          // @ts-expect-error: Let's ignore a compile error like this unreachable code 
-          <SyntaxHighlighter
-            style={materialDark}
-            PreTag="div"
-            language={match[1]}
-            children={String(children).replace(/\n$/, "")}
-            {...props}
-          />
-        ) : (
-          <code className={className ? className : ""} {...props}>
-            {children}
-          </code>
-        );
-      },
-      h1: HeadingBlock,
-      h2: HeadingBlock
-    };
-  
-    return (
-      <article className="prose prose-sm lg:prose-md">
-        <ReactMarkdown
-          components={components}
-          children={content}
-          remarkPlugins={[remarkGfm]}
-        />
-      </article>
-    );
+  const components = {
+    code({ node, inline, className, children, ...props }) {
+      const match = /language-(\w+)/.exec(className || "");
+
+      return !inline && match ? (
+        // @ts-expect-error: Let's ignore a compile error like this unreachable code 
+        <SyntaxHighlighter
+          style={materialDark}
+          PreTag="div"
+          language={match[1]}
+          {...props}
+          >
+          {String(children).replace(/\n$/, "")}
+        </SyntaxHighlighter>
+      ) : (
+        <code className={className ? className : ""} {...props}>
+          {children}
+        </code>
+      );
+    },
+    h1: HeadingBlock,
+    h2: HeadingBlock
   };
-  
-  export default Markdown;
+
+  return (
+    <article className="prose prose-sm lg:prose-md">
+      <ReactMarkdown
+        components={components}
+        remarkPlugins={[remarkGfm]}
+        >
+        {content}
+      </ReactMarkdown>
+    </article>
+  );
+};
+
+export default Markdown;
--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Html, Head, Main, NextScript } from 'next/document';
 
-
 function Document() {
   return (
     <Html lang="en">
